fe1e5c557d22501b7c31f16670c11b5fd2a805a5
Choccy-vr <94711504+Choccy-vr@users.noreply.github.com>
2024-08-21 08:41:43 -0400

    Update screen-firmware.sh
    Now switches display_connector to dev and adds to the display_connector.cfg file

--- screen-firmware.sh
+++ screen-firmware.sh
@@ -4,12 +4,29 @@ echo "Updating display firmware"
 echo " "
 echo "DO NOT power off the machine! This may take a while."
 #update screen firmware
-# Create and activate a Python virtual environment
+
 #stopping display service
 echo "Stopping display service"
 sudo service display stop
 echo " "
 echo "Stopped display service"
+
+#Make sure display_connector is on dev
+DISPLAY_CONNECTOR_DIR="${HOME}/display_connector"
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" = "main" ]; then
+    echo "You are on the main branch. Switching to dev."
+    local branch_name="dev"
+    local repo_dir="DISPLAY_CONNECTOR_DIR"
+    if [ -d "$repo_dir" ]; then
+        git -C "$repo_dir" reset --hard >/dev/null 2>&1
+        git -C "$repo_dir" clean -fd >/dev/null 2>&1
+        git -C "$repo_dir" checkout "$branch_name" >/dev/null 2>&1 && echo -e "${G}Switched $repo_dir to $branch_name.${NC}"
+    fi
+    
+fi
+
+# Create and activate a Python virtual environment
 #venv
 echo " "
 echo "Creating and activating a virtual environment..."
@@ -33,6 +50,11 @@ echo "Making necessary modifications"
 curl -o "${HOME}/display_connector/src/response_actions.py" https://raw.githubusercontent.com/OpenNeptune3D/display_firmware/main/Modified_Scripts/response_actions.py
 curl -o "${HOME}/display_connector/src/neptune4.py" https://raw.githubusercontent.com/OpenNeptune3D/display_firmware/main/Modified_Scripts/neptune4.py
 curl -o "${HOME}/display_connector/src/openneptune_display.py" https://raw.githubusercontent.com/OpenNeptune3D/display_firmware/main/Modified_Scripts/openneptune_display.py
+#modify display_connector.cfg
+config_file="${HOME}/printer_data/config/display_connector.cfg"
+new_line="display_type = openneptune"
+sed -i "/\[general\]/a $new_line" "$config_file"
+#done
 echo " "
 echo " Updated display firmware successfully. Restarting"
 sudo reboot
